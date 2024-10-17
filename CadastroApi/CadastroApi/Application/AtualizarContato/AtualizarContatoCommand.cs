77bf84d92b63a172c04d03094c7a420024a25db2
Diego Feijó <diego.feijo@hotmail.com>
2024-10-16 21:22:41 -0300

    feat: atualizar contato

---
+++ CadastroApi/CadastroApi/Application/AtualizarContato/AtualizarContatoCommand.cs
@@ -0,0 +1,10 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace CadastroApi.Application
+{
+    public class AtualizarContatoCommand : AdicionarContatoCommand
+    {
+        public Guid ID { get; set; }
+    }   
+}
---
+++ CadastroApi/UnitTest/AtualizarContatoTests.cs
@@ -0,0 +1,61 @@
+﻿using CadastroApi.Application;
+using CadastroApi.Models;
+using CadastroApi.Repository;
+using FluentAssertions;
+using Moq;
+
+namespace AtualizarContato
+{
+    public class AtualizarContatoTests
+    {
+        private readonly Mock<IContatoRepository> _contatoRepositoryMock;
+        private readonly AtualizarContatoCommandHandler _handler;
+
+        public AtualizarContatoTests()
+        {
+            _contatoRepositoryMock = new Mock<IContatoRepository>();
+            _handler = new AtualizarContatoCommandHandler(_contatoRepositoryMock.Object);            
+        }
+
+        [Fact]
+        public async Task AtualizarContato_DeveAtualizarContato()
+        {
+            var contatoId = Guid.NewGuid();
+
+            var command = new AtualizarContatoCommand
+            {
+                ID = contatoId,
+                Nome = "Batman",
+                DDD = "99",
+                Telefone = "999999999",
+                Email = "batman@gotham.com"
+            };
+
+            var result = await _handler.Handle(command, CancellationToken.None);
+
+            result.Should().Be(contatoId);
+
+            _contatoRepositoryMock.Verify(repo => repo.UpdateContatoAsync(It.IsAny<Contato>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task AtualizarContato_DeveGerarErro_ArgumentException()
+        {
+            var command = new AtualizarContatoCommand
+            {
+                Nome = "Batman",
+                DDD = "99",
+                Telefone = "999999999",
+                Email = "batman@gotham.com"
+            };
+
+            _contatoRepositoryMock.Setup(repo => repo.UpdateContatoAsync(It.IsAny<Contato>())).ThrowsAsync(new ArgumentException());
+
+            var act = async () => await _handler.Handle(command, CancellationToken.None);
+
+            await act.Should().ThrowAsync<ArgumentException>();
+
+            _contatoRepositoryMock.Verify(repo => repo.UpdateContatoAsync(It.IsAny<Contato>()), Times.Once);
+        }
+    }
+}
